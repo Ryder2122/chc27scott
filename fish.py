aee8e14c8ea2b53b3d81451db95165d32e21cd5b
main <ScottR27@chcstudent.com>
2025-09-24 14:10:26 -0400

    .

--- fish.py
+++ fish.py
@@ -4,5 +4,14 @@ print("1. carnivorous")
 print("2. salt water")
 print("3. communiuty")
 option = input("enter your fish numbe:")
-if option 
-
+if option == "1" :
+    
+    option =input("you already have it")
+    if (option == "yes"):
+        print("to bad") 
+    if(option == "no"):
+        print("enjoy") 
+    elif(option == "no"):
+        print("enjoy")
+elif option == "2":
+    option == print ('youre a fancy fish parent')
