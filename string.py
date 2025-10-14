b44ecd02845ff68c1ab28f2e48ae27c021e8e512
main <>
2025-10-14 13:20:31 -0400

    Create string.py

---
+++ string.py
@@ -0,0 +1,68 @@
+mystr = "hello"
+
+for char in mystr:
+    print(char)
+
+    print("for i loop")
+for i in range(len(mystr)):
+    print(mystr[i])                                          
+
+
+
+
+
+
+
+    #template program 
+    print("1. A")
+    print("2. B")
+    print("3. C")   
+    print("4. d")
+    option = input("enter your option:")
+    if option == "A":
+        print("executing A")
+    elif option == "B":
+        print("exucting B")     
+    elif option == "C":
+        print("executing C") 
+    elif option == "D":
+            print("executing D")    
+            
+
+
+
+
+
+
+
+
+
+
+
+
+            mystr="      A" 
+            print(mystr) 
+            mystr=mystr.strip()
+            print(mystr) 
+
+            optiojn = input("enter your selection:")
+            if option == "A":
+                print("you executing A")
+            elif option == "B":
+                print("you executing B")
+            elif option == "C":
+                print("you executing C")
+            elif option == "D":
+                print("you executing D")
+         
+    
+
+
+
+    print ("1. transfer") 
+    print ("2. deposit")
+    print ("3 . withdraw")
+    print ("quit") 
+
+    option = input("ENTER YOUR SELECTION:")
+              
