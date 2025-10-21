dd610ff788a15a2080d7c41ebb92bf339a1befb4
main <proeschero26@chcstudent.com>
2025-10-21 11:48:00 -0400

    .

--- strings2.py
+++ strings2.py
@@ -1,9 +1,37 @@
-mystr = "frank teauge preston" #all of my names are in on string but seperated by spaces 
+"""  
+Foundations of Computer Science PSET 2 – String Manipulation
+Problem 1 (count.py)
+Given the string below
+Str = “abbabababbabababababbababababababaaaaababbbbbbababababaababababababababbababaabababaabbabababbabababababbababababababaaaaababbbbbbababababaababababababababbababaabababaabbabababbabababababbababababababaaaaababbbbbbababababaababababababababbababaabababaaaaaababbbbbbababababaababbababbabababbababbabababababbababababababaaaaababbbbbbababababaababababababababbababaabababaabbabababbabababababbababbabababababababbababaabababaabbabababbabababababbababababababaaaaababbbbbbababababaabababababababaabababbababababababaaaaababbbbbbababababaababababababababbababaabababaaaaaababbbbbbababababaababbababbabababbababbbbbbababababaababababababababbababaabababaabbabababbabababababbababababababaaaaababbbbbbababababaababababababababbababaabababaaaaaababbbbbbababababaababbababbabababababababaababbababbabababbababbabababababbababababababaaaaababbbbbbababababaababababababababbababaabababaabbabababbabababababbababbabababababababbababaabababaabbabababbabbababababababaaaaababbbbbbababababaababababababababbababaabababaabbabababbabababababbababababababaaaaababbbbbbababababaababababababababbababaabababaaaaaababbbbbbababababaababbababbabababbababbabababababbababababababaaaaababbbbbbababababaababababababababbababaabababaabbabababbabababababbababbabababababababbababaabababaabbabababbababa”
+
+Count how many individual a’s and b’s there are, and print the counts.
+
+Problem 2 (mushrooms.py)
+Write a program that: 
+•	Asks for the size of a mushroom repeatedly until they type stop
+•	Sorts each mushroom into three separate lists, small, medium, large
+o	Small mushrooms are mushrooms less than 100 in size
+o	Medium mushrooms are mushrooms between 100 and 200 in size
+o	Large mushrooms are mushrooms greater than or equal to 200 in size.
+•	Print out the lists of each sized mushroom.
+
+Make sure you parse the input appropriately so you don’t get value or type errors, and make sure you parse the input so you typos don’t cause runtime errors. 
+Problem 3 (palindrome.py)
+A word is considered a palindrome if it is spelled the same forwards and backwards. For example,
+
+“noon”, “racecar”, “taco cat” are palindromes, while “school”, “class”, “room” are not palindromes. 
+
+Your goal is to write a program that can detect if an inputted word is a palindrome. Your program should parse the input such that “Taco Cat” evaluates to true, “        noon” evaluates to true, etc. 
+
+Submission:
+Submit each file individually on blackbaud. 
 
 
-names = mystr.split()       #this will split the string into a list of elemensts whihc each element is seperated by by the whitespaces 
-print(names)                    
 
+
+
+
+"""
 
 
 
