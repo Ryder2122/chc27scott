dda7df8027ac6c77afce3b223f55ea8090561382
main <proeschero26@chcstudent.com>
2025-11-17 11:49:22 -0500

    Update stockmarket.py

--- stockmarket.py
+++ stockmarket.py
@@ -1,19 +1,13 @@
 file = open("days.text","r")
 buffer = file.readlines() 
-file.close ()
 msft = buffer[0]
 print (buffer)                          
 print  = (buffer[0])
 
 
+msft = buffer[0].split(",") 
+msft.pop(0) 
+amzn = buffer[1] 
+nvda = buffer[2] 
 
-
-
-
-
-
-
-
-
-
-
+print(msft) 
