87ddcc75a047bc09568267ccc783b4130ab8d4f2
main <proeschero26@chcstudent.com>
2025-12-03 13:20:10 -0500

    .

--- rock, paper, sicssors.py
+++ rock, paper, sicssors.py
@@ -1,31 +1,62 @@
 # semester project: rock, paper, sicssors 
 import random
 
-actions = ["rock","paper", "sicssors"]
+choices = ["rock", "paper", "scissors"]
+
+while True:
+
+    user_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+
+
+    if user_choice not in choices:
+        print("Invalid choice. Please choose rock, paper, or scissors.")
+
+
+
+    computer_choice = random.choice(choices)
+    print(f"\nYou chose: {user_choice}, Computer chose: {computer_choice}\n")
+
+
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif (user_choice == "rock" and computer_choice == "scissors"):
+     (user_choice == "scissors" and computer_choice == "paper")
+    (user_choice == "paper" and computer_choice == "rock")
+    print("You win!")
+else: #this will run if its a tie and you did not win 
+    print("You lose!")   
+        
+
+
+    play_again = input("\nDo you want to play again? (yes please/nah im chilling): ").lower()
+    if play_again != "yes please":
+                print("Thanks for playing!")
+
+
+    actions = ["rock","paper", "sicssors"]
 
 user_choice = input( "enter your chocie (rock, paper, sicssors)")
 if user_choice not in actions:
-    print("invalid choice.please try agian") 
+                print("invalid choice.please try agian") 
 
 computer_choice = random.choice(actions) 
 print(f"\nYou chose {user_choice}, computer chose {computer_choice}.\n")
 
 if user_choice == computer_choice : 
-    print(" It's a tie ")
+                print(" It's a tie ")
 elif user_choice == "rock":
-    if computer_choice == "scissors":
-        print("Rock smashes scissors! You win!")
-elif:
-    print("Paper covers rock! You lose.")
-else: user_choice == "paper":
+                if computer_choice == "scissors":
+                    print("Rock smashes scissors! You win!")
+        
+print("Paper covers rock! You lose.")
+user_choice == "paper"
 if computer_choice == "rock":
-    print("Paper covers rock! You win!")
-elif:
+                print("Paper covers rock! You win!")
+        
 print("Scissors cuts paper! You lose.")
-else: user_choice == "scissors":
+user_choice == "scissors"
 if computer_choice == "paper":
-print("Scissors cuts paper! You win!")
-elif:
-print("Rock smashes scissors! You lose.")
-else:
-print("Invalid input. Please choose rock, paper, or scissors.")
+            print("Scissors cuts paper! You win!")
+        
+
+ 
