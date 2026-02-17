9392122b3df1a5c7aa0ad712bee35ae598bfb8eb
main <proeschero26@chcstudent.com>
2026-02-17 14:10:08 -0500

    .

---
+++ lab4.py
@@ -0,0 +1,10 @@
+# Ryder scott statistics calculator
+
+
+import math                                                                                                                                                              
+def getlist():
+    pass 
+print(" welcome to the list statistics calculator")
+print(" enter a list of integers of 'q' to end the list")
+check = True
+while check == True: 
